c27d4bed0a73a67a6e71f0436981efb4a55b9bad
Mohammed El Saeed <67608414+moSaeed15@users.noreply.github.com>
2024-05-18 22:25:34 +0300

    added logout redirect

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { signOut } from 'next-auth/react';
+import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
 
 const LogoutButton = () => {
   return (
-    <button
-      onClick={() => signOut()}
-      className="btn bg-brand-red hover:bg-red-600 text-white mt-auto transition-colors "
-    >
-      <CiLogout size={20} color="white" /> Log out
-    </button>
+    <Link href="/">
+      <button
+        onClick={() => signOut()}
+        className="btn bg-brand-red hover:bg-red-600 text-white mt-auto transition-colors "
+      >
+        <CiLogout size={20} color="white" /> Log out
+      </button>
+    </Link>
   );
 };
 
